ddc8093878e5fe83bf3b1cdc11b8b697b2339ee7
Alisha <alishananda@gmail.com>
2017-01-21 23:05:34 -0800

    Added nav bar

---
+++ app/src/main/java/com/example/www/thenetwork/Newsfeed.java
@@ -0,0 +1,23 @@
+package com.example.www.thenetwork;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alish on 1/21/2017.
+ */
+
+public class Newsfeed extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.activity_project, container, false);
+        return myView;
+    }
+}
---
+++ app/src/main/java/com/example/www/thenetwork/PersonalAccount.java
@@ -0,0 +1,23 @@
+package com.example.www.thenetwork;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alish on 1/21/2017.
+ */
+
+public class PersonalAccount extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.personal_account, container, false);
+        return myView;
+    }
+}
